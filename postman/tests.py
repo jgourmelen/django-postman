b9342dfe02834837c721f078e472e9a9ab74877f
Patrick Samson <pk.samson@gmail.com>
2017-12-13 10:26:24 +0100

    Finalized package for tagged release

--- postman/tests.py
+++ postman/tests.py
@@ -82,7 +82,7 @@ class GenericTest(TestCase):
     Usual generic tests.
     """
     def test_version(self):
-        self.assertEqual(sys.modules['postman'].__version__, "3.6.0.post1")
+        self.assertEqual(sys.modules['postman'].__version__, "3.6.1")
 
 
 class TransactionViewTest(TransactionTestCase):
